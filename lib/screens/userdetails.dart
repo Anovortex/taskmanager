02dfac66418d69f68c9b859335dcfa278d00f296
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2024-04-27 23:25:49 +0600

    userdetails updated

--- lib/screens/userdetails.dart
+++ lib/screens/userdetails.dart
@@ -1,40 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:taskmanager/screens/reset_password.dart';
 import 'package:taskmanager/screens/signin_screen.dart';
 import 'package:taskmanager/utils/color_utils.dart';
 
-class UserDetails {
-  final String fullName;
-  final String email;
-  final String phoneNumber;
-
-  UserDetails({
-    required this.fullName,
-    required this.email,
-    required this.phoneNumber,
-  });
-}
-
 class UserDetailsForm extends StatefulWidget {
+  const UserDetailsForm({super.key});
+
   @override
-  _UserDetailsFormState createState() => _UserDetailsFormState();
+  State<UserDetailsForm> createState() => _UserDetailsFormState();
 }
 
 class _UserDetailsFormState extends State<UserDetailsForm> {
   final _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
 
-  late TextEditingController _fullNameController;
-  late TextEditingController _phoneNumberController;
+  // Controllers
+  final _fullNameController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _fullNameController = TextEditingController();
-    _phoneNumberController = TextEditingController();
-
-    // Retrieve current user's details from Firestore
     _fetchUserDetails();
   }
 
@@ -45,8 +32,7 @@ class _UserDetailsFormState extends State<UserDetailsForm> {
     super.dispose();
   }
 
-  // Method to fetch user details from Firestore
-  void _fetchUserDetails() async {
+  Future<void> _fetchUserDetails() async {
     final currentUser = _auth.currentUser;
     if (currentUser != null) {
       try {
@@ -55,82 +41,47 @@ class _UserDetailsFormState extends State<UserDetailsForm> {
             .doc(currentUser.uid)
             .get();
 
-        // Update the text controllers with the existing user details
         if (userData.exists) {
-          setState(() {
-            _fullNameController.text = userData.get('fullName');
-            _phoneNumberController.text = userData.get('phoneNumber');
-          });
+          _fullNameController.text = userData.get('fullName');
+          _phoneNumberController.text = userData.get('phoneNumber');
         }
-      } catch (error) {
-        print('Failed to fetch user details: $error');
+      } catch (e) {
+        debugPrint('Failed to fetch user details: $e');
       }
     }
   }
 
-  void _saveUserDetails() async {
-    // Validate user input
-    if (_fullNameController.text.isEmpty ||
-        _phoneNumberController.text.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Please fill in all fields.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('OK'),
-              ),
-            ],
+  Future<void> _saveUserDetails() async {
+    if (_formKey.currentState!.validate()) {
+      final currentUser = _auth.currentUser;
+      if (currentUser != null) {
+        try {
+          await FirebaseFirestore.instance
+              .collection('userdetails')
+              .doc(currentUser.uid)
+              .set({
+            'fullName': _fullNameController.text,
+            'email': currentUser.email,
+            'phoneNumber': _phoneNumberController.text,
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('User details saved successfully.')),
           );
-        },
-      );
-      return;
-    }
-
-    // Get the current user
-    final currentUser = _auth.currentUser;
-    if (currentUser == null) {
-      // Handle scenario where current user is null (user not signed in)
-      return;
-    }
-
-    // Save user details to Firestore
-    try {
-      await FirebaseFirestore.instance
-          .collection('userdetails')
-          .doc(currentUser.uid)
-          .set({
-        'fullName': _fullNameController.text,
-        'email': currentUser.email,
-        'phoneNumber': _phoneNumberController.text,
-      });
-
-      // Show a success message or navigate to another screen
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('User details saved successfully.'),
-        ),
-      );
-    } catch (error) {
-      print('Failed to save user details: $error');
+        } catch (e) {
+          debugPrint('Failed to save user details: $e');
+        }
+      }
     }
   }
 
-  // Method to handle user logout
-  void _logout() async {
+  Future<void> _logout() async {
     try {
       await _auth.signOut();
-      // Navigate to the login screen
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => SignInScreen()),
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
       );
-    } catch (error) {
-      print('Failed to log out: $error');
+    } catch (e) {
+      debugPrint('Failed to log out: $e');
     }
   }
 
@@ -138,16 +89,15 @@ class _UserDetailsFormState extends State<UserDetailsForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Details'),
+        title: const Text('User Details'),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
             onPressed: _logout,
           ),
         ],
-        backgroundColor: hexStringToColor(
-            "627254"), // Set the background color to transparent
-        elevation: 0, // Remove the shadow
+        backgroundColor: hexStringToColor("627254"),
+        elevation: 0,
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
@@ -167,30 +117,46 @@ class _UserDetailsFormState extends State<UserDetailsForm> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextField(
-                  controller: _fullNameController,
-                  decoration: InputDecoration(labelText: 'Full Name'),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Email: ${_auth?.currentUser?.email ?? "SEx"}',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: _phoneNumberController,
-                  decoration: InputDecoration(labelText: 'Phone Number'),
-                ),
-                SizedBox(height: 32.0),
-                ElevatedButton(
-                  onPressed: _saveUserDetails,
-                  child: Text('Save'),
-                ),
-                SizedBox(height: 32.0),
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    controller: _fullNameController,
+                    decoration: const InputDecoration(labelText: 'Full Name'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your full name';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    'Email: ${_auth.currentUser?.email ?? ""}',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _phoneNumberController,
+                    decoration:
+                        const InputDecoration(labelText: 'Phone Number'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your phone number';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: _saveUserDetails,
+                    child: const Text('Save'),
+                  ),
+                  const SizedBox(height: 32.0),
+                ],
+              ),
             ),
           ),
         ),
